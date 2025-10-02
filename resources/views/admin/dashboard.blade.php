c9679495380b28a142075a1f720aade72b2e8fca
Aman Ullah <amanullahshakoor284@gmail.com>
2025-10-02 17:42:04 +0500

    new change

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -39,7 +39,7 @@
                                         <div class="row ">
                                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                                                 <div class="card-content">
-                                                    <h5 class="font-15">Weekly Revenue</h5>
+                                                    <h5 class="font-15">Weekly Revenuessss</h5>
                                                     <h2 class="mb-3 font-18">${{ number_format($weeklyRevenue, 2) }}
                                                     </h2>
 
